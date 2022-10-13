b215ae0e4b855675c86a5bd423c28fee355030b1
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-13 10:02:17 +0530

    passed test_get_countries

--- StockAPI/main.py
+++ StockAPI/main.py
@@ -1,5 +1,5 @@
-import investpy
-from fastapi import FastAPI,status
+from investpy import stocks
+from fastapi import FastAPI,HTTPException,status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -17,3 +17,12 @@ app.add_middleware(
 @app.get("/")
 def root():
     return JSONResponse(status_code=status.HTTP_200_OK,content="Hello World")
+
+@app.get("/countries")
+def get_countries(sortby:str="asce"):
+    if sortby not in ['asce','desc']:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="query:sortby must be 'asce' or 'desc'")
+    countries = stocks.get_stock_countries()
+    if sortby == "desc":
+        countries.reverse()
+    return countries
--- StockAPI/test_main.py
+++ StockAPI/test_main.py
@@ -1,4 +1,4 @@
-import investpy
+from investpy import stocks
 from main import app
 from fastapi import status
 from fastapi.testclient import TestClient
@@ -10,3 +10,22 @@ def test_root():
     res = client.get("/")
     assert res.status_code == status.HTTP_200_OK
     assert res.json() == "Hello World"
+
+def test_get_countries():
+    # 1
+    countries = stocks.get_stock_countries()
+    res = client.get("/countries")
+    assert res.status_code == status.HTTP_200_OK
+    res_body = res.json()
+    assert countries == res_body
+    # 2
+    res = client.get("/countries?sortby=desc")
+    assert res.status_code == status.HTTP_200_OK
+    countries.reverse()
+    res_body = res.json()
+    assert countries == res_body
+    # 3
+    res = client.get("/countries?sortby=xyz")
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    res_body = res.json()
+    assert res_body['detail'] == "query:sortby must be 'asce' or 'desc'"
